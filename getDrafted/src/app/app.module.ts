7b5fd334e7bb07fc1b45618a20e759c2fbeb309e
salvgrd <salvgrd@gmail.com>
2019-08-26 22:34:38 +0200

    Cambios implementados en appModule.ts y appComponent.html

--- getDrafted/src/app/app.module.ts
+++ getDrafted/src/app/app.module.ts
@@ -3,22 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { BodyComponent } from './body/body.component';
-=======
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
->>>>>>> ea7dc6191cb627172a29ba78c318bb142db53ded
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    BodyComponent
-=======
     HeaderComponent,
-    HeroComponent
->>>>>>> ea7dc6191cb627172a29ba78c318bb142db53ded
+    HeroComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
