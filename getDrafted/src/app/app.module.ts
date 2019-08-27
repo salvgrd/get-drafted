badbd3fded8a31811e29b96dddd74be516d1d123
salvgrd <salvgrd@gmail.com>
2019-08-27 14:22:07 +0200

    Componente Footer creado

--- getDrafted/src/app/app.module.ts
+++ getDrafted/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HeroComponent,
-    BodyComponent
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
