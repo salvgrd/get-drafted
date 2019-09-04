5562bed63aa94f1321cea6cd4429e37e8c7bcab2
salvgrd <salvgrd@gmail.com>
2019-09-04 21:26:43 +0200

    Scroll desde navbar implementado

--- getDrafted/src/app/landing/landing.component.ts
+++ getDrafted/src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute, private router: Router ) { }
 
   ngOnInit() {
+  }
+  onAnchorClick ( ) {
+    this.route.fragment.subscribe ( f => {
+      const element = document.querySelector( "#" + f )
+      if(element)element.scrollIntoView({block: "start", behavior: "smooth"})
+    });
   }
 
 }
