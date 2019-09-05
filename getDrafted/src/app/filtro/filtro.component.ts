bc97636c9bde3bf5ced3d699997b48597d408183
salvgrd <salvgrd@gmail.com>
2019-09-05 17:40:19 +0200

    Filtrado por nombre terminado

--- getDrafted/src/app/filtro/filtro.component.ts
+++ getDrafted/src/app/filtro/filtro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-//import { Atleta } from '../models/atleta.model';
 import { AtletasService } from '../atletas.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,10 +9,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./filtro.component.css']
 })
 export class FiltroComponent implements OnInit {
-
+  filters: FormGroup;
+  arrResults: any;
   arrAtletas: any;
 
-  constructor( private atletasService: AtletasService, private router: Router) { }
+  constructor( private atletasService: AtletasService, private router: Router) {
+    this.filters = new FormGroup({
+      usertype: new FormControl ('', []),
+      nombre: new FormControl ('', []),
+      pais: new FormControl ('', []),
+      disci: new FormControl ('', []),
+      sexo: new FormControl ('', [])
+    })
+  }
 
   ngOnInit() {
     this.atletasService.getAll()
@@ -20,13 +29,23 @@ export class FiltroComponent implements OnInit {
         if (response['error']) {
           alert(response['error']);
         } else {
-           this.arrAtletas = response;
-           console.log(response);
+           this.arrResults = response;
         }
       })
       .catch((err) => {
         console.log(err);
       })
-  } 
+  }
+  onSearch(){
+    this.arrAtletas = [];
+    if (this.filters.value.nombre != ""){
+      let fname = this.filters.value.nombre;
+      let atletas = this.arrResults;
+      for (let atleta of atletas){
+        if (atleta.nombre == fname) this.arrAtletas.push(atleta);
+        else console.log(`v1: ${atleta.nombre} y v2: ${fname} no coinciden`)
+      }
+    }
+  }
 
 }
