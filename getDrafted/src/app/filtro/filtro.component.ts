4d93db3c54ba7b1f3ccf500be760424443ee3074
salvgrd <salvgrd@gmail.com>
2019-09-06 21:13:57 +0200

    Filtrado de formulario implementado.

--- getDrafted/src/app/filtro/filtro.component.ts
+++ getDrafted/src/app/filtro/filtro.component.ts
@@ -36,15 +36,56 @@ export class FiltroComponent implements OnInit {
         console.log(err);
       })
   }
+  // Obtener de estas funciones un dato, y utilizar el conjunto de datos para hacer un filtrado complejo.
+  filterByName(){
+    let fname = this.filters.value.nombre;
+    let atletas = this.arrResults;
+    let filteredByName = atletas.filter( atleta => atleta.nombre == fname )
+    return filteredByName;
+    // for (let atleta of atletas){
+    //   if (atleta.nombre == fname) return atleta.nombre this.arrAtletas.push(atleta);
+    //   else return fname
+    // }
+  }
+  filterByLocation(arr){
+    let pArr = arr;
+    if(pArr.length == 0) pArr = this.arrResults;
+    let fubicacion = this.filters.value.pais;
+    let filteredByLocation = pArr.filter(atleta => atleta.ubicacion == fubicacion);
+    return filteredByLocation;
+  }
+  filterBySport(arr){
+    let pArr = arr;
+    if(pArr.length == 0) pArr = this.arrResults;
+    let fdisci = this.filters.value.disci;
+    let filteredBySport = pArr.filter(atleta => atleta.disciplina == fdisci);
+    console.log(pArr);
+    return filteredBySport;
+  }
+  filterByGender(arr){
+    let pArr = arr;
+    if(pArr.length == 0) pArr = this.arrResults;
+    let fsexo = this.filters.value.sexo;
+    let filteredByGender = pArr.filter(atleta => atleta.sexo == fsexo);
+    return filteredByGender;
+  }
   onSearch(){
     this.arrAtletas = [];
-    if (this.filters.value.nombre != ""){
-      let fname = this.filters.value.nombre;
-      let atletas = this.arrResults;
-      for (let atleta of atletas){
-        if (atleta.nombre == fname) this.arrAtletas.push(atleta);
-        else console.log(`v1: ${atleta.nombre} y v2: ${fname} no coinciden`)
-      }
+    if (this.filters.value.nombre != "") {
+      let fNombre = this.filterByName()
+      this.arrAtletas = fNombre;
+    };
+    if (this.filters.value.pais != "") {
+      let filtered = this.filterByLocation(this.arrAtletas)
+      this.arrAtletas = filtered;
+    };
+    if (this.filters.value.disci != "") {
+      let filtered = this.filterBySport(this.arrAtletas)
+      this.arrAtletas = filtered;
+    }
+    if (this.filters.value.sexo != "") {
+      let filtered = this.filterByGender(this.arrAtletas)
+      this.arrAtletas = filtered;
     }
   }
 
