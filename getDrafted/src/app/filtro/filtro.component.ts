945ba9d4ae8dd9215f010820a79e2e2d98e2b1b3
salvgrd <salvgrd@gmail.com>
2019-09-07 15:22:45 +0200

    Maquetacion e inicializacion del filtrado de sponsors

--- getDrafted/src/app/filtro/filtro.component.ts
+++ getDrafted/src/app/filtro/filtro.component.ts
@@ -9,18 +9,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./filtro.component.css']
 })
 export class FiltroComponent implements OnInit {
-  filters: FormGroup;
+  show: boolean;
+  filtersAtletas: FormGroup;
+  filtersSponsor: FormGroup;
   arrResults: any;
   arrAtletas: any;
 
   constructor( private atletasService: AtletasService, private router: Router) {
-    this.filters = new FormGroup({
-      usertype: new FormControl ('', []),
+    this.filtersAtletas = new FormGroup({
       nombre: new FormControl ('', []),
       pais: new FormControl ('', []),
       disci: new FormControl ('', []),
       sexo: new FormControl ('', [])
     })
+    this.filtersSponsor = new FormGroup({
+      nombre: new FormControl ('', []),
+      pais: new FormControl ('', []),
+      disci: new FormControl ('', [])
+    })
+    this.show = true;
   }
 
   ngOnInit() {
@@ -36,28 +43,25 @@ export class FiltroComponent implements OnInit {
         console.log(err);
       })
   }
-  // Obtener de estas funciones un dato, y utilizar el conjunto de datos para hacer un filtrado complejo.
+  // Hacer estas funciones escalables.
+  // funciones formulario de atletas
   filterByName(){
-    let fname = this.filters.value.nombre;
+    let fname = this.filtersAtletas.value.nombre;
     let atletas = this.arrResults;
     let filteredByName = atletas.filter( atleta => atleta.nombre == fname )
     return filteredByName;
-    // for (let atleta of atletas){
-    //   if (atleta.nombre == fname) return atleta.nombre this.arrAtletas.push(atleta);
-    //   else return fname
-    // }
   }
   filterByLocation(arr){
     let pArr = arr;
     if(pArr.length == 0) pArr = this.arrResults;
-    let fubicacion = this.filters.value.pais;
+    let fubicacion = this.filtersAtletas.value.pais;
     let filteredByLocation = pArr.filter(atleta => atleta.ubicacion == fubicacion);
     return filteredByLocation;
   }
   filterBySport(arr){
     let pArr = arr;
     if(pArr.length == 0) pArr = this.arrResults;
-    let fdisci = this.filters.value.disci;
+    let fdisci = this.filtersAtletas.value.disci;
     let filteredBySport = pArr.filter(atleta => atleta.disciplina == fdisci);
     console.log(pArr);
     return filteredBySport;
@@ -65,28 +69,29 @@ export class FiltroComponent implements OnInit {
   filterByGender(arr){
     let pArr = arr;
     if(pArr.length == 0) pArr = this.arrResults;
-    let fsexo = this.filters.value.sexo;
+    let fsexo = this.filtersAtletas.value.sexo;
     let filteredByGender = pArr.filter(atleta => atleta.sexo == fsexo);
     return filteredByGender;
   }
   onSearch(){
     this.arrAtletas = [];
-    if (this.filters.value.nombre != "") {
+    if (this.filtersAtletas.value.nombre != "") {
       let fNombre = this.filterByName()
       this.arrAtletas = fNombre;
     };
-    if (this.filters.value.pais != "") {
+    if (this.filtersAtletas.value.pais != "") {
       let filtered = this.filterByLocation(this.arrAtletas)
       this.arrAtletas = filtered;
     };
-    if (this.filters.value.disci != "") {
+    if (this.filtersAtletas.value.disci != "") {
       let filtered = this.filterBySport(this.arrAtletas)
       this.arrAtletas = filtered;
     }
-    if (this.filters.value.sexo != "") {
+    if (this.filtersAtletas.value.sexo != "") {
       let filtered = this.filterByGender(this.arrAtletas)
       this.arrAtletas = filtered;
     }
   }
+  //funciones del formulario de sponsors.
 
 }
